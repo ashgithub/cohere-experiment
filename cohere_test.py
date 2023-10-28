6d8afeca1e8340120d78deaadb6a34128dac60c2
Ashish Agarwal <agarwal.ash@gmail.com>
2023-10-28 18:58:15 +0000

    cohere & oci test

---
+++ cohere_test.py
@@ -0,0 +1,15 @@
+import cohere
+import streamlit as st
+
+st.title("Cohere test")
+co = cohere.Client('rN5X4fIbAtckJzZhsVMosXMcVjjPb0ZR1NtXNzLN') # This is your trial API key
+response = co.generate(
+  model='command',
+  prompt='hello, its very hot, what will you suggest i do to get comfortable ? give me a list of 5 things in order of effectiveness and estimate the $ value\n',
+  max_tokens=300,
+  temperature=0.9,
+  k=0,
+  stop_sequences=[],
+  return_likelihoods='NONE')
+st.write('Prediction: {}'.format(response.generations[0].text))
+print('Prediction: {}'.format(response.generations[0].text))
